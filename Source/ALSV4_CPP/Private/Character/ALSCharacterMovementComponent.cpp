0ab967416bb906db01eb7112708deb2884184ace
Greynz <dr.dangers@gmail.com>
2020-12-22 17:51:17 -0800

    save ALSV4 Ironstrike work

--- Source/ALSV4_CPP/Private/Character/ALSCharacterMovementComponent.cpp
+++ Source/ALSV4_CPP/Private/Character/ALSCharacterMovementComponent.cpp
@@ -3,16 +3,16 @@
 // Original Author: Haziq Fadhil
 // Contributors:    Doga Can Yanikoglu
 
-
 #include "Character/ALSCharacterMovementComponent.h"
 #include "Character/ALSBaseCharacter.h"
 
-UALSCharacterMovementComponent::UALSCharacterMovementComponent(const FObjectInitializer& ObjectInitializer)
+UALSCharacterMovementComponent::UALSCharacterMovementComponent(const FObjectInitializer &ObjectInitializer)
 	: Super(ObjectInitializer)
 {
+	DodgeStrength = 500.f;
 }
 
-void UALSCharacterMovementComponent::OnMovementUpdated(float DeltaTime, const FVector& OldLocation, const FVector& OldVelocity)
+void UALSCharacterMovementComponent::OnMovementUpdated(float DeltaTime, const FVector &OldLocation, const FVector &OldVelocity)
 {
 	Super::OnMovementUpdated(DeltaTime, OldLocation, OldVelocity);
 
@@ -21,12 +21,55 @@ void UALSCharacterMovementComponent::OnMovementUpdated(float DeltaTime, const FV
 		return;
 	}
 
-	// Set Movement Settings
+	// Movespeed
 	if (bRequestMovementSettingsChange)
 	{
 		MaxWalkSpeed = MyNewMaxWalkSpeed;
 		MaxWalkSpeedCrouched = MyNewMaxWalkSpeed;
 	}
+
+	//Dodge
+	if (bWantsToDodge)
+	{
+		bWantsToDodge = false;
+
+		//Only dodge if on the ground (in the air causes problems trying to get the two modes to line up due to friction)
+		if (IsMovingOnGround())
+		{
+			MoveDirection.Normalize();
+			FVector DodgeVelocity = MoveDirection * DodgeStrength;
+			//Set Z component to zero so we don't go up
+			DodgeVelocity.Z = 0.0f;
+
+			Launch(DodgeVelocity);
+		}
+	}
+
+	//Predicted Launch
+	if (bWantsToLaunch)
+	{
+		bWantsToLaunch = false;
+		Launch(LaunchVector);
+	}
+}
+
+// Override of base CMC launch -- different than our predicted launch
+bool UALSCharacterMovementComponent::HandlePendingLaunch()
+{
+	if (!PendingLaunchVelocity.IsZero() && HasValidData())
+	{
+		Velocity = PendingLaunchVelocity;
+		//Remmed out so our dodge move won't play the falling animation.
+		if (Velocity.Z > 0.0f)
+		{
+			SetMovementMode(MOVE_Falling);
+		}
+		PendingLaunchVelocity = FVector::ZeroVector;
+		bForceNextFloorCheck = true;
+		return true;
+	}
+
+	return false;
 }
 
 void UALSCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags) // Client only
@@ -34,15 +77,17 @@ void UALSCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags) // C
 	Super::UpdateFromCompressedFlags(Flags);
 
 	bRequestMovementSettingsChange = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+	bWantsToDodge = (Flags & FSavedMove_Character::FLAG_Custom_1) != 0;
+	bWantsToLaunch = (Flags & FSavedMove_Character::FLAG_Custom_2) != 0;
 }
 
-class FNetworkPredictionData_Client* UALSCharacterMovementComponent::GetPredictionData_Client() const
+class FNetworkPredictionData_Client *UALSCharacterMovementComponent::GetPredictionData_Client() const
 {
 	check(PawnOwner != nullptr);
 
 	if (!ClientPredictionData)
 	{
-		UALSCharacterMovementComponent* MutableThis = const_cast<UALSCharacterMovementComponent*>(this);
+		UALSCharacterMovementComponent *MutableThis = const_cast<UALSCharacterMovementComponent *>(this);
 
 		MutableThis->ClientPredictionData = new FNetworkPredictionData_Client_My(*this);
 		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
@@ -57,6 +102,10 @@ void UALSCharacterMovementComponent::FSavedMove_My::Clear()
 	Super::Clear();
 
 	bSavedRequestMovementSettingsChange = false;
+	bSavedWantsToDodge = false;
+	bSavedWantsToLaunch = false;
+	SavedMoveDirection = FVector::ZeroVector;
+	SavedLaunchVector = FVector::ZeroVector;
 }
 
 uint8 UALSCharacterMovementComponent::FSavedMove_My::GetCompressedFlags() const
@@ -68,23 +117,76 @@ uint8 UALSCharacterMovementComponent::FSavedMove_My::GetCompressedFlags() const
 		Result |= FLAG_Custom_0;
 	}
 
+	if (bSavedWantsToDodge)
+	{
+		Result |= FLAG_Custom_1;
+	}
+
+	if (bSavedWantsToLaunch)
+	{
+		Result |= FLAG_Custom_2;
+	}
+
 	return Result;
 }
 
-void UALSCharacterMovementComponent::FSavedMove_My::SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel,
-                                                               class FNetworkPredictionData_Client_Character& ClientData)
+bool UALSCharacterMovementComponent::FSavedMove_My::CanCombineWith(const FSavedMovePtr &NewMove, ACharacter *Character, float MaxDelta) const
+{
+	//Set which moves can be combined together. This will depend on the bit flags that are used.
+	if (bSavedRequestMovementSettingsChange != ((FSavedMove_My *)&NewMove)->bSavedRequestMovementSettingsChange)
+	{
+		return false;
+	}
+	if (bSavedWantsToDodge != ((FSavedMove_My *)&NewMove)->bSavedWantsToDodge)
+	{
+		return false;
+	}
+	if (bSavedWantsToLaunch != ((FSavedMove_My *)&NewMove)->bSavedWantsToLaunch)
+	{
+		return false;
+	}
+	if (SavedMoveDirection != ((FSavedMove_My *)&NewMove)->SavedMoveDirection)
+	{
+		return false;
+	}
+	if (SavedLaunchVector != ((FSavedMove_My *)&NewMove)->SavedLaunchVector)
+	{
+		return false;
+	}
+
+	return Super::CanCombineWith(NewMove, Character, MaxDelta);
+}
+
+void UALSCharacterMovementComponent::FSavedMove_My::SetMoveFor(ACharacter *Character, float InDeltaTime, FVector const &NewAccel,
+															   class FNetworkPredictionData_Client_Character &ClientData)
 {
 	Super::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
 
-	UALSCharacterMovementComponent* CharacterMovement = Cast<UALSCharacterMovementComponent>(Character->GetCharacterMovement());
+	UALSCharacterMovementComponent *CharacterMovement = Cast<UALSCharacterMovementComponent>(Character->GetCharacterMovement());
 	if (CharacterMovement)
 	{
 		bSavedRequestMovementSettingsChange = CharacterMovement->bRequestMovementSettingsChange;
+		bSavedWantsToDodge = CharacterMovement->bWantsToDodge;
+		bSavedWantsToLaunch = CharacterMovement->bWantsToLaunch;
+		SavedMoveDirection = CharacterMovement->MoveDirection;
+		SavedLaunchVector = CharacterMovement->LaunchVector;
+	}
+}
+
+void UALSCharacterMovementComponent::FSavedMove_My::PrepMoveFor(class ACharacter *Character)
+{
+	Super::PrepMoveFor(Character);
+
+	UALSCharacterMovementComponent *CharacterMovement = Cast<UALSCharacterMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+		CharacterMovement->MoveDirection = SavedMoveDirection;
+		CharacterMovement->LaunchVector = SavedLaunchVector;
 	}
 }
 
 UALSCharacterMovementComponent::FNetworkPredictionData_Client_My::FNetworkPredictionData_Client_My(
-	const UCharacterMovementComponent& ClientMovement)
+	const UCharacterMovementComponent &ClientMovement)
 	: Super(ClientMovement)
 {
 }
@@ -92,6 +194,15 @@ UALSCharacterMovementComponent::FNetworkPredictionData_Client_My::FNetworkPredic
 FSavedMovePtr UALSCharacterMovementComponent::FNetworkPredictionData_Client_My::AllocateNewMove()
 {
 	return MakeShared<FSavedMove_My>();
+}
+
+//Set Max Walk Speed RPC to transfer the current Max Walk Speed from the Owning Client to the Server
+bool UALSCharacterMovementComponent::Server_SetMaxWalkingSpeed_Validate(const float NewMaxWalkSpeed)
+{
+	if (NewMaxWalkSpeed < 0.f || NewMaxWalkSpeed > 2000.f)
+		return false;
+	else
+		return true;
 }
 
 void UALSCharacterMovementComponent::Server_SetMaxWalkingSpeed_Implementation(const float NewMaxWalkSpeed)
@@ -108,3 +219,53 @@ void UALSCharacterMovementComponent::SetMaxWalkingSpeed(float NewMaxWalkSpeed)
 	}
 	bRequestMovementSettingsChange = true;
 }
+
+// Dodge ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//Dodge RPC to transfer the current Move Direction from the Owning Client to the Server
+bool UALSCharacterMovementComponent::Server_MoveDirection_Validate(const FVector &MoveDir)
+{
+	return true;
+}
+
+void UALSCharacterMovementComponent::Server_MoveDirection_Implementation(const FVector &MoveDir)
+{
+	MoveDirection = MoveDir;
+}
+
+//Trigger the Dodge ability on the Owning Client
+void UALSCharacterMovementComponent::Dodge()
+{
+	if (PawnOwner->IsLocallyControlled())
+	{
+		MoveDirection = PawnOwner->GetLastMovementInputVector();
+		Server_MoveDirection(MoveDirection);
+	}
+
+	bWantsToDodge = true;
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END Dodge
+
+//Launch ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bool UALSCharacterMovementComponent::Server_SetLaunchVector_Validate(const FVector &LaunchVec)
+{
+	return (LaunchVec.Size() >= 0.f && LaunchVec.Size() <= 20000.f);
+}
+
+//Launch RPC to transfer the current Launch Vector from the Owning Client to the Server
+void UALSCharacterMovementComponent::Server_SetLaunchVector_Implementation(const FVector &LaunchVec)
+{
+	LaunchVector = LaunchVec;
+}
+
+//Trigger the Dodge ability on the Owning Client
+void UALSCharacterMovementComponent::PredictedLaunch(const FVector &LaunchVec)
+{
+	if (PawnOwner->IsLocallyControlled())
+	{
+		LaunchVector = LaunchVec;
+		Server_SetLaunchVector(LaunchVector);
+	}
+
+	bWantsToLaunch = true;
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END Launch
--- Source/ALSV4_CPP/Public/Character/ALSCharacterMovementComponent.h
+++ Source/ALSV4_CPP/Public/Character/ALSCharacterMovementComponent.h
@@ -20,22 +20,31 @@ class ALSV4_CPP_API UALSCharacterMovementComponent : public UCharacterMovementCo
 	class FSavedMove_My : public FSavedMove_Character
 	{
 	public:
-
 		typedef FSavedMove_Character Super;
 
+		virtual bool CanCombineWith(const FSavedMovePtr &NewMove, ACharacter *Character, float MaxDelta) const override;
 		virtual void Clear() override;
 		virtual uint8 GetCompressedFlags() const override;
-		virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel,
-		                        class FNetworkPredictionData_Client_Character& ClientData) override;
+		virtual void SetMoveFor(ACharacter *Character, float InDeltaTime, FVector const &NewAccel,
+								class FNetworkPredictionData_Client_Character &ClientData) override;
+		virtual void PrepMoveFor(class ACharacter *Character) override;
 
 		// Walk Speed Update
 		uint8 bSavedRequestMovementSettingsChange : 1;
+
+		//Dodge
+		FVector SavedMoveDirection;
+		uint8 bSavedWantsToDodge : 1;
+
+		//Dodge
+		FVector SavedLaunchVector;
+		uint8 bSavedWantsToLaunch : 1;
 	};
 
 	class FNetworkPredictionData_Client_My : public FNetworkPredictionData_Client_Character
 	{
 	public:
-		FNetworkPredictionData_Client_My(const UCharacterMovementComponent& ClientMovement);
+		FNetworkPredictionData_Client_My(const UCharacterMovementComponent &ClientMovement);
 
 		typedef FNetworkPredictionData_Client_Character Super;
 
@@ -43,11 +52,10 @@ class ALSV4_CPP_API UALSCharacterMovementComponent : public UCharacterMovementCo
 	};
 
 public:
-
+	virtual bool HandlePendingLaunch() override;
 	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
-	virtual class FNetworkPredictionData_Client* GetPredictionData_Client() const override;
-	virtual void OnMovementUpdated(float DeltaTime, const FVector& OldLocation, const FVector& OldVelocity) override;
-
+	virtual class FNetworkPredictionData_Client *GetPredictionData_Client() const override;
+	virtual void OnMovementUpdated(float DeltaTime, const FVector &OldLocation, const FVector &OldVelocity) override;
 
 	// Movement Settings Variables
 	uint8 bRequestMovementSettingsChange = 1;
@@ -58,6 +66,32 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Movement Settings")
 	void SetMaxWalkingSpeed(float NewMaxWalkSpeed);
 
-	UFUNCTION(Reliable, Server)
+	UFUNCTION(Reliable, Server, WithValidation)
 	void Server_SetMaxWalkingSpeed(float NewMaxWalkSpeed);
+
+	//Dodge ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	UPROPERTY(EditAnywhere, Category = "Dodge")
+	float DodgeStrength;
+
+	UFUNCTION(Unreliable, Server, WithValidation)
+	void Server_MoveDirection(const FVector &MoveDir);
+
+	//Trigger the dodge ability (Called from the owning client)
+	UFUNCTION(BlueprintCallable, Category = "Dodge")
+	void Dodge();
+
+	FVector MoveDirection;
+	uint8 bWantsToDodge : 1;
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End Dodge
+
+	// launch ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	UFUNCTION(BlueprintCallable, Category = "Movement Settings")
+	void PredictedLaunch(const FVector &LaunchVec);
+
+	UFUNCTION(Reliable, Server, WithValidation)
+	void Server_SetLaunchVector(const FVector &LaunchVec);
+
+	FVector LaunchVector;
+	uint8 bWantsToLaunch : 1;
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End launch
 };
